eab2c7b5dc6dc6156ca3df395c64148999a2c3bf
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-03-18 21:49:00 -0300

    Agora o exemplo da busca linear completo....

--- src/App.java
+++ src/App.java
@@ -1,6 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Criar um array chamado eramosSeis com o valores pré-definidos..
+        int eramosSeis[] = {9, 4, 63, 999, -5, -16};
+        int buscar = 999;
+
+        int resultado = linearSearch(eramosSeis, buscar);
+        // Mostra quando achou!
+        if (resultado > -1){
+            System.out.println("Achei o "+buscar+"! O índice é: "+resultado);
+        } else {
+            System.out.println("Não achei o "+buscar+"!!!");
+        }
+
+        int buscar2 = 16;
+        resultado = linearSearch(eramosSeis, buscar2);
+        // Mostra quando achou!
+        if (resultado > -1){
+            System.out.println("Achei o "+buscar2+"! O índice é: "+resultado);
+        } else {
+            System.out.println("Não achei o "+buscar2+"!!!");
+        }
+
     }
 
     // Função para busca linear...
