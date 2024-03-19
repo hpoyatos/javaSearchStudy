d2c53994db0393b98ec5296433e33c369222e1ab
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-03-18 21:00:23 -0300

    Busca Linear codada, mas não testada ou usada...

--- src/App.java
+++ src/App.java
@@ -2,4 +2,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    // Função para busca linear...
+    public static int linearSearch(int arr[], int x)
+    {
+        int n = arr.length;
+
+        //Percorrer o array
+        for(int i=0; i<n; i++){
+            if (arr[i] == x){
+                // Retorna o índice caso tenha achado!
+                return i;
+            }
+        }
+        // Retorna -1 quando NÃO ACHOU!
+        return -1;
+    }
+
 }
