b5e212a62bd1f029fb99d4e7c67a907c9afa646e
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-03-25 21:24:02 -0300

    Busca por "árvore binária" concluída...

--- src/App.java
+++ src/App.java
@@ -21,6 +21,9 @@ public class App {
             System.out.println("Não achei o "+buscar2+"!!!");
         }
 
+        int array25[] = { 17, 20, 26, 31, 44, 54, 55, 65, 77, 93 };
+        resultado = binaryTreeSearch(array25, 5);
+        System.out.println(resultado);
     }
 
     // Função para busca linear...
@@ -36,6 +39,28 @@ public class App {
             }
         }
         // Retorna -1 quando NÃO ACHOU!
+        return -1;
+    }
+
+    // Função para busca na estratégia "árvore binária"
+    public static int binaryTreeSearch(int arr[], int x) {
+        int n = arr.length;
+        int l = 0; // esquerdo
+        int r = n - 1;
+
+        // Enquanto for possível "estreitar a árvore"
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            // Se o número procurado estiver BEM no meio, ele retornará o índice (mid)
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] > x) { // número buscado é menor!
+                r = mid - 1;
+            } else { // número buscado é maior!
+                l = mid + 1;
+            }
+        }
         return -1;
     }
 
