1ce7538b5713998577335aa8f076751d76a8a500
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-03-25 21:50:47 -0300

    Teste de performance, mas se assegure que os dois arrays (desordenador e ordenado) sejam iguais.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        /* 
         // Criar um array chamado eramosSeis com o valores pré-definidos..
         int eramosSeis[] = {9, 4, 63, 999, -5, -16};
         int buscar = 999;
@@ -23,7 +26,38 @@ public class App {
 
         int array25[] = { 17, 20, 26, 31, 44, 54, 55, 65, 77, 93 };
         resultado = binaryTreeSearch(array25, 5);
+        //System.out.println(resultado);
+        */
+        int quantidade = 1000;
+        int[] vetor = new int[quantidade];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random()*quantidade);
+        }
+
+        long tempoInicial = System.currentTimeMillis();
+        int resultado = linearSearch(vetor, 777);
         System.out.println(resultado);
+        long tempoFinal = System.currentTimeMillis();
+
+        System.out.println("Busca Linear executada em = " + (tempoFinal - tempoInicial) + " ms");
+
+ 
+
+        int[] vetor_ordenado = bubbleSort(vetor);
+
+        tempoInicial = System.currentTimeMillis();
+        resultado = binaryTreeSearch(vetor_ordenado, 190);
+        System.out.println(resultado);
+        tempoFinal = System.currentTimeMillis();
+
+        System.out.println("Busca 'árvore binária' executada em = " + (tempoFinal - tempoInicial) + " ms");
+
+
+
+
+
+
     }
 
     // Função para busca linear...
@@ -64,4 +98,20 @@ public class App {
         return -1;
     }
 
+    private static int[] bubbleSort(int vetor[]){
+        boolean troca = true;
+        int aux;
+        while (troca) {
+            troca = false;
+            for (int i = 0; i < vetor.length - 1; i++) {
+                if (vetor[i] > vetor[i + 1]) {
+                    aux = vetor[i];
+                    vetor[i] = vetor[i + 1];
+                    vetor[i + 1] = aux;
+                    troca = true;
+                }
+            }
+        }
+        return vetor;
+    }
 }
